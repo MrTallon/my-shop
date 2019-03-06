a894455beacf343266c1b99ed8c23808424ba7fa
MrTallon <352420160@qq.com>
2019-03-06 23:53:51 +0800

    :sparkles:swagger实时文档

--- myshop-service-reg/src/main/java/com/funtl/myshop/service/reg/MyShopServiceRegApplication.java
+++ myshop-service-reg/src/main/java/com/funtl/myshop/service/reg/MyShopServiceRegApplication.java
@@ -3,11 +3,13 @@ package com.funtl.myshop.service.reg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan(basePackages = "com.funtl.myshop.commons.mapper")
+@EnableSwagger2
 public class MyShopServiceRegApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyShopServiceRegApplication.class, args);
