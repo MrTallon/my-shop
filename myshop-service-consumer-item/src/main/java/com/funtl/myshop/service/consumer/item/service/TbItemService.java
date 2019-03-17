19d389db38c34f8f2a9ef89d8e5c33ad8cc713ab
MrTallon <352420160@qq.com>
2019-03-17 22:08:06 +0800

    :art:将商品服务生产者改为post请求

--- myshop-service-consumer-item/src/main/java/com/funtl/myshop/service/consumer/item/service/TbItemService.java
+++ myshop-service-consumer-item/src/main/java/com/funtl/myshop/service/consumer/item/service/TbItemService.java
@@ -1,11 +1,12 @@
 package com.funtl.myshop.service.consumer.item.service;
 
+import com.funtl.myshop.commons.domain.TbItem;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
- * This is Description
+ * 创建服务消费者
  *
  * @author YangBo
  * @date 2019/03/10
@@ -13,6 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(value = "myshop-service-provider-item")
 public interface TbItemService {
 
-    @GetMapping(value = "/item/page/{num}/{size}")
-    String page(@PathVariable(name = "num") int pageNum, @PathVariable(name = "size") int pageSize);
+    @PostMapping(value = "/item/page/{pageNum}/{pageSize}")
+    String page(TbItem tbItem, @PathVariable(name = "pageNum") int pageNum, @PathVariable(name = "pageSize") int pageSize);
 }
